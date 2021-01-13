f70619522eca32d11d99245c55ac8c4f52d42480
rishiar4 <therishabhsharma763@gmail.com>
2021-01-13 19:09:25 +0530

    updating with lists
    Signed-off-by: rishiar4 <therishabhsharma763@gmail.com>

---
+++ r1.py
@@ -0,0 +1,141 @@
+# print("Hello World")
+# print("New Line")
+
+# This is a comment
+
+"""
+this is multi line
+"""
+
+# a = 5
+# b = 5.0
+# c = True
+
+# print( type(c) )
+# print( type(c) )
+
+# str = "Rishabh"
+
+# str.charAt(0)
+
+#  Slicing
+# print( str )
+# print( str[ 4 ] )
+# print( str[ 0:4 ] ) 
+
+# print( str[ -5:-1 ] )
+
+
+# def fn( f ):
+#     a = 10
+#     print("Calling a function")
+#     print(a)
+#     b = 10
+#     return a + b + f
+
+# c = fn( 50 )
+
+# # c = 100000000000000000000000000000000000000000000
+
+# # c = c ** 8
+
+# print( c )
+
+# for( int i = 2; i < 10; i++ ){
+#     cout<<i;
+# } 
+
+# for i in range(10):
+#     print(i)
+
+# for i in range( 2, 10, 2 ):
+#     print(i)
+
+# for i in range( 10, 2, -4 ):
+#     print(i)
+
+i = 9
+if i > 10:
+    print(i)
+elif i == 0:
+    print("Zero")
+else: print("Lower")
+
+# sum = 0
+# for i in range( 1, 10 ):
+#     print( i * 5 )
+#     sum = sum + i * 5
+
+# print( sum )
+
+
+# def f2():
+#     sum = 0
+#     for i in range( 1, 10 ):
+#         sum = sum + i
+#     return sum
+
+# print( f2() )
+
+# i = 5
+# while( i < 10 ):
+#     print( i )
+#     i = i + 1
+
+
+# Tuple, Dict, List, Set
+
+#  List
+# print( l )
+
+# print( l[0] + 12 )
+
+# print( l[1] )
+
+# print( l[2] + "12" )
+
+# print( len(l) )
+
+# for i in range( len(l) ):
+#     print( l[i] )
+
+# for i in l:
+#     print(i)
+
+# l2 = [ 5, 9, "abc" ]
+
+# # l2 = l2 + ["tg"]
+
+# l2.append( "tg" )
+
+# print( l2 )
+
+f1 = [ 0, 5, 8 ]
+
+# f1 = f1 * 5 
+# Output 
+# [0, 5, 8, 0, 5, 8, 0, 5, 8, 0, 5, 8, 0, 5, 8]
+
+f1[1] = f1[1] * 5
+
+# print( f1 )
+
+l3 = [ 1, 5, 67 ]
+
+l4 = [ 8, 10, 45 ]
+
+l5 = l4 + l3
+
+# print( l5 )
+
+# print( l5[ 0 : 4] )
+
+# print( l5[-1])
+
+# print( l5 )
+
+# print( l5.pop(5) )
+
+
+# print( l5.pop(4) )
+# print( l5 )
